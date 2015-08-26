1e0d28b1b2afce6eaeaeb495e6094b7777951393
max <maxwellfundi@live.com>
2015-08-26 09:51:51 +0300

    this is the second trial with variables declared with option explicit on

--- VBTest Project/WindowsApplication1/frmregistration.vb
+++ VBTest Project/WindowsApplication1/frmregistration.vb
@@ -1,4 +1,7 @@
 ﻿Option Explicit On
+Dim f_name As String
+Dim l_name As String
+Dim email As String
 
 Public Class frmRegistration
     Private Sub frmRegistration_Load(sender As Object, e As EventArgs) Handles MyBase.Load
